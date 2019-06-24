36cae0eeab5251e8a8a6418ec9a5c461483670fc
Tracy Reuther <tracyreuther@gmail.com>
2019-06-24 22:41:22 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,10 +35,7 @@ def position_taken?(index)
 end
 
 def valid_move?(index)
-    if (index.between?(0,8)) && position_taken?(index) == false
-      true
-    else nil
-  end
+    index.between?(0,8) && !position_taken?(index)
 end
 
 def turn_count
