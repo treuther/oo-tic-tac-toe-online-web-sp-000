d2b89501d863297c2c636ec283229504d6886b2a
Tracy Reuther <tracyreuther@gmail.com>
2019-06-24 22:10:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -47,7 +47,7 @@ def turn
   input = gets.strip
   index = input_to_index(input)
     if valid_move?(index)
-      move(index, token)
+      move(index, token = "X")
       display_board
     else
     turn
